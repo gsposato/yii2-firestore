892d8a17bf271f1f09baa89a7913a9dc0c036dea
Gregory Sposato <gsposato@mastrack.com>
2018-06-06 14:56:27 -0400

    updating to use google/cloud-firestore instead

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # yii2-firestore
 A Yii2 Component that connects to Google Firestore
+
+Before using this plugin, run this command on your server:
+
+```bash
+    pecl install protobuf
+```
+
+In your main-local:
+
+```php
+        'firestore' => [
+            'class' => 'vendor\gsposato\yii2firestore\FirestoreComponent',
+            'credential_file'=>'/path/to/your/firebase_auth.json',
+            'project' => 'your-project-name',
+        ],
+```
+
+Code Examples:
+
+```php
+    /**
+     * Firestore Add
+     */
+    public function actionUnitFirestoreAdd()
+    {
+        $person = array(
+            "name" => "Gregory Sposato"
+        );
+
+        print_r(Yii::$app->firestore->add('people', $person)); // this generates an $id for other functions
+    }
+
+    /**
+     * Firestore Get
+     * @param String $id
+     */
+    public function actionUnitFirestoreGet($id)
+    {
+        print_r(Yii::$app->firestore->get('people', $id));
+    }
+
+    /**
+     * Firestore Update
+     * @param String $id
+     */
+    public function actionUnitFirestoreUpdate($id)
+    {
+        $arr = array(
+            "name" => "Gregory Sposato Jr.",
+            "nested-data" => array(
+                "age" => "28",
+                "hair-color" => "brown",
+                "eye-color" => "brown",
+                "height" => "5ft, 2in"
+            )
+        );
+        print_r(Yii::$app->firestore->update('people', $id, $arr));
+    }
+
+    /**
+     * Unit Test for Firestore Remove
+     * @param String $id
+     */
+    public function actionUnitFirestoreRemove($id)
+    {
+        print_r(Yii::$app->firestore->remove('people', $id)); // this actually doesn't work yet
+    }
+
+    /**
+     * Queries Firestore
+     */
+    public function actionQueryFirestore()
+    {
+        $conditions = array(
+
+                array(
+                    "property" => "timestamp",
+                    "operator" => ">=",
+                    "comparison" => Yii::$app->firestore->timestamp(new \DateTime("Mon Jan 1st 2018"))
+                    ),
+
+                array(
+                    "property" => "timestamp",
+                    "operator" => "<=",
+                    "comparison" => Yii::$app->firestore->timestamp( new \DateTime("now"))
+                    )
+                );
+
+        print_r(Yii::$app->firestore->query("people", $id, "visits", $conditions));
+    }
+```
+
+
--- src/FirestoreComponent.php
+++ src/FirestoreComponent.php
@@ -2,10 +2,60 @@
 
 namespace gsposato\yii2firestore;
 
+use Yii;
+use \yii\web\View;
+use GuzzleHttp\Client;
+use common\helpers\MastrackHelper;
+use \Google\Cloud\Core\Timestamp;
+use \Google\Cloud\Datastore\DatastoreClient;
+use \Google\Cloud\Firestore\FirestoreClient;
+use \Google\Cloud\Firestore\DocumentReference;
+use \Google\Cloud\Firestore\Transaction as FirestoreTransaction;
+
 class FirestoreComponent extends \yii\base\Component
 {
-    public $project_key;
-    public $api_key;
+    public $project;
+    public $credential_file;
+
+    /**
+     * Initialize other functions
+     */
+    public function init()
+    {
+        $config = array(
+            "projectId" => $this->project,
+            "keyFile" => json_decode(file_get_contents($this->credential_file), true)
+        );
+
+        return new FirestoreClient($config);
+    }
+
+    /**
+     * Create a GeoPoint
+     * @param float $latitude
+     * @param float $longitude
+     * @return Object
+     */
+    public function geopoint($latitude,$longitude)
+    {
+        $config = array(
+            "projectId" => $this->project,
+            "keyFile" => json_decode(file_get_contents($this->credential_file), true)
+        );
+
+        $datastore = new DatastoreClient($config);
+        return $datastore->geoPoint($latitude, $longitude);
+    }
+
+    /**
+     * Create a Timestamp
+     * @param DateTime $datetime
+     * @return Object
+     */
+    public function timestamp($datetime)
+    {
+        return new Timestamp($datetime);
+    }
 
     /**
      * Get firestore document
@@ -14,9 +64,17 @@ class FirestoreComponent extends \yii\base\Component
      */
     public function get($collection_name, $document_name)
     {
-        $client = $this->client();
-        $document = $this->document();
-        return $client->getDocument($collection_name, $document_name);
+        $firestore = $this->init();
+
+        try 
+        {
+            return $firestore->document($collection_name . '/' . $document_name);
+        }
+
+        catch (\Exception $e)
+        {
+            return false;
+        }
     }
 
     /**
@@ -26,27 +84,37 @@ class FirestoreComponent extends \yii\base\Component
      */
     public function add($collection_name, $data)
     {
-        $client = $this->client();
-        $document = $this->document();
- 
-        foreach ($data as $key => $value)
-        {
-            $document->setString($key, $value);
-        }
-
-        return $client->addDocument($collection_name, $document);
+        $firestore = $this->init();
+        $collection = $firestore->collection($collection_name);
+        $new = $collection->add($data);
+        return $new->id();
     }
 
     /**
      * Update firestore document
      * @param String $collection_name
      * @param String $document_name
+     * @param Array $data
      */
-    public function update($collection_name, $document_name)
+    public function update($collection_name, $document_name, $data)
     {
-        $client = $this->client();
-        $document = $this->document();
-        return $client->updateDocument($collection_name, $document_name, $document, true);
+        $firestore = $this->init();
+        $collection = $firestore->collection($collection_name)->document($document_name);
+        $fieldPaths = array();
+
+        foreach ($data as $key => $value)
+        {
+            if (is_array($value))
+            {
+                array_push($fieldPaths, ["path" => $firestore->fieldPath([$key]),"value" => $value]);
+                continue;
+            }
+
+            array_push($fieldPaths, ["path" => $key, "value" => $value]);
+        }
+
+        $result = $collection->update($fieldPaths);
+        return $result;
     }
 
     /**
@@ -56,26 +124,105 @@ class FirestoreComponent extends \yii\base\Component
      */
     public function remove($collection_name, $document_name)
     {
-        $client = $this->client();
-        $document = $this->document();
-        return $client->deleteDocument($collection_name, $document_name);
+        $firestore = $this->init();
+        $document = $firestore->collection($collection_name)->document($document_name);
+        $document->delete();
     }
 
     /**
-     * Initialize firestore client wrapper
+     * Remove all firestore documents by property
+     * @param String $collection_name
+     * @param String $property_name
+     * @param String $property_value
+     * @param Boolean $debug
      */
-    protected function client()
+    public function removeByProperty($collection_name, $organization_name, $sub_collection_name, $property_name, $property_value)
     {
-        return new FirestoreApiClient(
-            $this->project_key, $this->api_key
-        );
+        $firestore = $this->init();
+        $collection = $firestore->collection($collection_name)->document($organization_name)->collection($sub_collection_name);
+        $query = $collection->where($property_name, '=', $property_value);
+        $documents = $query->documents();
+
+        foreach ($documents as $document)
+        {
+            if (!$document->exists())
+            {
+                echo "document does not exist.\n";
+                continue;
+            }
+
+            echo "deleting document: " . $document->name . "\n";
+            $document->delete();
+            sleep(10);
+        }
     }
 
     /**
-     * Initialize firestore document wrapper
+     * Get a group of firestore documents based on specific conditions
+     * @param String $collection_name
+     * @param Array $conditions
+     * @return Object
      */
-    protected function document()
+    public function query($superCollection, $organizationKey, $subCollection, $conditions = array())
     {
-        return new FirestoreDocument();
+        if (empty($conditions))
+        {
+            throw new \Exception("Query must contain at least one condition");
+        }
+
+        $firestore = $this->init();
+        $collection = $firestore->collection($superCollection)->document($organizationKey)->collection($subCollection);
+
+        switch (count($conditions))
+        {
+
+        case 1:
+            $query = $collection
+                ->where(
+                    $conditions[0]["property"],
+                    $conditions[0]["operator"], 
+                    $conditions[0]["comparison"]
+                );
+            break;
+
+        case 2:
+            $query = $collection
+                ->where(
+                    $conditions[0]["property"],
+                    $conditions[0]["operator"], 
+                    $conditions[0]["comparison"]
+                )
+                ->where(
+                    $conditions[1]["property"],
+                    $conditions[1]["operator"], 
+                    $conditions[1]["comparison"]
+                );
+            break;
+
+        case 3:
+            $query = $collection
+                ->where(
+                    $conditions[0]["property"],
+                    $conditions[0]["operator"], 
+                    $conditions[0]["comparison"]
+                )
+                ->where(
+                    $conditions[1]["property"],
+                    $conditions[1]["operator"], 
+                    $conditions[1]["comparison"]
+                )
+                ->where(
+                    $conditions[2]["property"],
+                    $conditions[2]["operator"], 
+                    $conditions[2]["comparison"]
+                );
+            break;
+
+        default:
+            throw new \Exception("Component can only compare 3 properties at a time.");
+
+        }
+
+        return $query->documents();
     }
 }
